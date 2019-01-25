978af78d6f2f6d2164a041c6e954bf0c0056503d
Vinicius Carvalho <vcarvalho@team.mobile.de>
2019-01-25 13:49:35 +0100

    Create zuul-server

---
+++ zuul-server/src/main/java/com/app/zuulserver/configuration/ProxyApi.java
@@ -0,0 +1,39 @@
+package com.app.zuulserver.configuration;
+
+import java.util.stream.Collectors;
+
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import lombok.AllArgsConstructor;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+
+@Configuration
+@AllArgsConstructor
+public class ProxyApi {
+
+    private ZuulProperties properties;
+
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider() {
+        return () -> properties
+            .getRoutes()
+            .values()
+            .stream()
+            .map(route -> createResource(route.getServiceId(), route.getId(), "2.0"))
+            .collect(Collectors.toList());
+    }
+
+    private SwaggerResource createResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation("/" + location + "/v2/api-docs");
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
